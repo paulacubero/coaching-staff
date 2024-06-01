654cece7bf537a1668861ed47c0d60a9e2b9d684
paulacubero <paulacubero.e@gmail.com>
2024-06-01 20:19:57 +0200

    back

---
+++ server/controllers/authController.js
@@ -0,0 +1,23 @@
+const User = require("../models/users");
+const jwt = require("jsonwebtoken");
+
+const secret = "superadmin";
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).send("Usuario no encontrado");
+    }
+    if (user.password !== password) {
+      return res.status(401).send("Contraseña incorrecta");
+    }
+    const token = jwt.sign({ id: user._id, role: user.role }, secret, {
+      expiresIn: "1h",
+    });
+    res.send({ token });
+  } catch (error) {
+    res.status(500).send("Error del servidor");
+  }
+};
